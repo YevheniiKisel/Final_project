219781e4d4c9aeff5b4e218b77443088d5467390
yevheniikisel <yevhenii.kisel.design@icloud.com>
2022-08-05 21:57:55 +0200

    Checking if main.subscription working

--- project/main.py
+++ project/main.py
@@ -1,6 +1,9 @@
+from ast import Sub
+from crypt import methods
 from unicodedata import name
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Subscription, Delivery_address
 from . import db
 
 main = Blueprint('main', __name__)
@@ -17,6 +20,35 @@ def profile():
 @main.route('/subscription')
 def subscription():
     return render_template("subscription1.html")
+
+@main.route('/subscription', methods = ["POST"])
+@login_required
+def subscription_post():
+    #data for Subscribtion table in database
+    deliveryPerWeek = request.form.get('perWeek')
+    subscriptionPeriod = request.form.get('durationMonth')
+    deliveryDate = request.form.get('deliveryDate')
+    deliveryTime = request.form.get('deliveryTime')
+    starterPack = request.form.get('starterPack')
+
+    #data for Delivery table in database
+    str = request.form.get('steet')
+    blck = request.form.get('block')
+    apart = request.form.get('apartment')
+    contactNumber = request.form.get('contactNumber')
+    
+    #create a new subscription record with data above
+    new_subscripton = Subscription(user=current_user.id, delivery_PerWeek=deliveryPerWeek, subscription_Period=subscriptionPeriod, delivery_Date=deliveryDate, delivery_Time=deliveryTime, starter_Pack=starterPack)
+    new_subscripton.save()
+
+    #create a new delivery record with data above
+    new_delivery = Delivery_address(user=current_user.id, street=str, block=blck, apartment=apart, contact_Number=contactNumber)
+    new_delivery.save()
+
+    #pass flash, that said, that purchase was successful 
+    flash("Purchase was successful!")
+
+    return render_template("profile.html", )
 
 @main.route('/bouquet')
 def bouquet():
